27196a7107aaa05565d5d3f22ba403be1e8f4265
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-18 01:24:32 -0400

    Add some methods to the GPG class
    new methods:
    1. exist_enc
    2. exist_dec
    3. encrypt
    4. decrypt
    5. __get_enc_filename
    6. __get_dec_filename

--- ggit/core/gpg.py
+++ ggit/core/gpg.py
@@ -1,11 +1,15 @@
+import os
 from subprocess import PIPE
 from subprocess import run
 
 
 class GPG:
 
+    # encrypted file extension
+    EXT = ".gpg"
+
     def __init__(self):
-        self.__get_gpg_id()
+        self.__get_id()
 
     def __exec_gpg(self, args=[]):
         cmd = ["gpg"] + args
@@ -14,10 +18,51 @@ class GPG:
             raise RuntimeError(r.stderr.decode("utf-8").rstrip())
         return r.stdout.decode("utf-8")
 
-    def __get_gpg_id(self):
+    def __get_id(self):
         resp = self.__exec_gpg(["--list-secret-keys", "--with-colons"])
         for line in resp.splitlines():
             if not line.startswith("sec"):
                 continue
-            self.id = line.split(":")[4]
+            self.__id = line.split(":")[4]
             break
+
+    def __get_enc_filename(self, file):
+        return file + self.EXT
+
+    def __get_dec_filename(self, file):
+        return file.replace(self.EXT, "")
+
+    def exist_enc(self, file):
+        return os.path.isfile(self.__get_enc_filename(file))
+
+    def exist_dec(self, file):
+        return os.path.isfile(self.__get_dec_filename(file))
+
+    def encrypt(self, file):
+        if not os.path.isfile(file):
+            raise ValueError(f"file not found: {file}")
+
+        self.__exec_gpg([
+            "--use-agent",
+            "--yes",
+            "--trust-model=always",
+            "-r", self.__id,
+            "--encrypt",
+            "-o", self.__get_enc_filename(file),
+            file
+        ])
+
+    def decrypt(self, file):
+        if not os.path.isfile(file):
+            raise ValueError(f"file not found: {file}")
+
+        if not file.endswith(self.EXT):
+            raise ValueError(f"file type not supported: {file}")
+
+        self.__exec_gpg([
+            "--use-agent",
+            "-q",
+            "--decrypt",
+            "-o", self.__get_dec_filename(file),
+            file
+        ])
