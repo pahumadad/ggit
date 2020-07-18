437328121f50f85b92e8285ea16c6edf75fce280
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-18 01:35:21 -0400

    Add the Git class with the status method

---
+++ ggit/core/git.py
@@ -0,0 +1,21 @@
+from subprocess import PIPE
+from subprocess import run
+
+
+class Git:
+
+    def __exec_git(self, args=[]):
+        cmd = ["git"] + args
+        r = run(cmd, stdout=PIPE, stderr=PIPE)
+        if r.returncode != 0:
+            raise RuntimeError(r.stderr.decode("utf-8").rstrip())
+        return r.stdout.decode("utf-8")
+
+    def status(self):
+        out = []
+        resp = self.__exec_git(["status"])
+        for line in resp.splitlines():
+            if line == "":
+                break
+            out.append(line)
+        return out
