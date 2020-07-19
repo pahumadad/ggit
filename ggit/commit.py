9d4f4db9a8c6f4c13b57101998f6945fe79da967
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-19 00:28:04 -0400

    Update the commit function to handle modified files

--- ggit/commit.py
+++ ggit/commit.py
@@ -1,4 +1,8 @@
+from ggit.core.common import get_diff
+from ggit.core.common import get_files
+from ggit.core.common import FILE_TYPE_NO_ENC
 from ggit.core.git import Git
+from ggit.core.gpg import GPG
 
 
 def commit(files, message, all):
@@ -8,6 +12,16 @@ def commit(files, message, all):
     args.extend(["--message", message])
     if all:
         args.extend(["--all"])
+
+        # look for all modifed files
+        files = get_files(file_type=FILE_TYPE_NO_ENC)
+        gpg = GPG()
+        for f in files:
+            if not gpg.exist_enc(f):
+                continue
+            if not get_diff(f):
+                continue
+            gpg.encrypt(f)
 
     git = Git()
     git.commit(args)
