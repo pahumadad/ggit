1d263edc015f4501279b567d8f41e64795393d90
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-18 23:41:32 -0400

    Rename the file arg from the commit function as files

--- ggit/commit.py
+++ ggit/commit.py
@@ -1,10 +1,10 @@
 from ggit.core.git import Git
 
 
-def commit(file, message, all):
+def commit(files, message, all):
 
     # prepare commit args
-    args = file
+    args = files
     args.extend(["--message", message])
     if all:
         args.extend(["--all"])
