c4993ac551743eeaf79196bd537af84efb1184e0
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-18 23:42:37 -0400

    Update the GPG class
    * the get_enc_file and get_dec_file private methods are now publics.
    * the decrypt method can now create the decrypted file in a temp folder
      when the 'tmp' arg is set as True.
    * the decrypt method returns the decrypted file path.

--- ggit/core/gpg.py
+++ ggit/core/gpg.py
@@ -8,6 +8,9 @@ class GPG:
     # encrypted file extension
     EXT = ".gpg"
 
+    # tmp folder
+    TMP = "/tmp"
+
     def __init__(self):
         self.__get_id()
 
@@ -27,26 +30,31 @@ class GPG:
             break
 
     @classmethod
-    def __get_enc_file(cls, file):
+    def get_enc_file(cls, file):
         return file + cls.EXT
 
     @classmethod
-    def __get_dec_file(cls, file):
-        return file.replace(cls.EXT, "")
+    def get_dec_file(cls, file, tmp=False):
+        file = file.replace(cls.EXT, "")
+        if tmp:
+            name = os.path.basename(file)
+            return os.path.join(cls.TMP, name)
+        else:
+            return file
 
     @classmethod
     def exist_enc(cls, file):
-        return os.path.isfile(cls.__get_enc_file(file))
+        return os.path.isfile(cls.get_enc_file(file))
 
     @classmethod
     def exist_dec(cls, file):
-        return os.path.isfile(cls.__get_dec_file(file))
+        return os.path.isfile(cls.get_dec_file(file))
 
     def encrypt(self, file):
         if not os.path.isfile(file):
             raise ValueError(f"file not found: {file}")
 
-        enc_file = self.__get_enc_file(file)
+        enc_file = self.get_enc_file(file)
         self.__exec_gpg([
             "--use-agent",
             "--yes",
@@ -58,17 +66,19 @@ class GPG:
         ])
         return enc_file
 
-    def decrypt(self, file):
+    def decrypt(self, file, tmp=False):
         if not os.path.isfile(file):
             raise ValueError(f"file not found: {file}")
 
         if not file.endswith(self.EXT):
             raise ValueError(f"file type not supported: {file}")
 
+        dec_file = self.get_dec_file(file, tmp=tmp)
         self.__exec_gpg([
             "--use-agent",
             "-q",
             "--decrypt",
-            "-o", self.__get_dec_file(file),
+            "-o", dec_file,
             file
         ])
+        return dec_file
