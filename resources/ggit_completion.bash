3f372fdbd0ecda7fbbeb0f64bbdfeaccd7b460d3
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2020-07-22 22:48:50 -0400

    Improve the bash completion to complete file paths

--- resources/ggit_completion.bash
+++ resources/ggit_completion.bash
@@ -4,7 +4,17 @@ ggit_completion() {
     prev_arg="${COMP_WORDS[COMP_CWORD-1]}";
     curr_arg="${COMP_WORDS[COMP_CWORD]}";
 
-    if [[ "$prev_arg" == "ggit" ]]; then
+    if [[ "$prev_arg" == "add"  || "$prev_arg" == "commit" ||\
+          "$prev_arg" == "diff" || "$prev_arg" == "restore" ]]; then
+        local sug=($(compgen -W "$(fd --hidden --no-ignore-vcs --type f)" "$curr_arg"))
+
+        if [ "${#sug[@]}" == "1" ]; then
+            local envi=${sug[0]/%\ */}
+            COMPREPLY=("$envi")
+        else
+            COMPREPLY=("${sug[@]}")
+        fi
+    elif [[ "$prev_arg" == "ggit" ]]; then
         COMPREPLY=($(compgen -W "add commit diff push restore status" "${curr_arg//-/\\\-}"))
     fi
 }
